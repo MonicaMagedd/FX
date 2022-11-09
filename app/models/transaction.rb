758e9769ff4886ea63349887e6f8a624952aced4
Monica Maged <mmaged@uplandsoftware.com>
2022-11-09 16:48:22 +0200

    validates the input and output amount and currency

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+    validates :input_amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+    validates :input_currency, inclusion: { in: %w(EUR USD)}
+    validates :output_amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+    validates :output_currency, inclusion: { in: %w(EUR USD)}
+
 end
